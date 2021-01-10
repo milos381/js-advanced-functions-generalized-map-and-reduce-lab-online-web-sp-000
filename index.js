f68d0ad3c014cbd32755507b547ae4e49da1feec
milos381 <milos_popovic@outlook.com>
2021-01-09 19:03:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your functions here
+
+
+//cb is essentially a callback function.
+//we could see the first line of the code as:
+
+//function map(src, function(elementInIteration){...}) {
+//    let r = []
+//    ...
+//}
+
+//why we wrote it like this is because we can invoke
+//any function to operate on these numbers
+
+function map(src, cb) {
+    let r = []
+  
+    for (let i = 0; i < src.length; i++) {
+      let theElement = src[i]
+      r.push(cb(theElement))
+    }
+  
+    return r;
+  }
+
+  function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
